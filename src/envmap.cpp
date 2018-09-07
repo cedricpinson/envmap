f2f36054959cfc08301213772c220e9b6e91dd7c
Cedric Pinson <trigrou@trigrou.com>
2018-09-07 16:33:35 +0200

    Adds arguments

--- src/envmap.cpp
+++ src/envmap.cpp
@@ -6,51 +6,134 @@
 #include <stdio.h>
 #include <thread>
 
-static bool debug = false;
+#include <getopt.h>
+
+struct Options
+{
+    int numSamples = 1024;
+    int cubemapSize = 256;
+    const char* inputFilename;
+    int thumbnailSize = 256;
+    int nbThreads = 0;
+    bool debug = false;
+    bool quiet = false;
+    unsigned short padding;
+    unsigned int padding2;
+};
+
+void printUsage()
+{
+    const char* text =
+        "envmap is a tool to precompute environment for PBR rendering "
+        "usages:"
+        " envmap [options] panorama_env.hdr"
+        ""
+        "example"
+        " envmap -s 512 -n 4096 panorama_env.hdr"
+        ""
+        "Options:"
+        " -s [--ibl-size] to define the cubemap resolution computed, default 256"
+        " -n [--ibl-sample] number of sample to compute a texel, default 1024"
+        " -t [--thumbnail-size] width size of generated thumbnail, default 256, it will generate 256x128 thumbnail"
+        " -d a debug flag to write intermediate texture"
+        " -m [--no-threads] no multithreading, default is false";
+
+        printf("%s", text);
+}
+
+int parseArgument(Options& options, int argc, char** argv)
+{
+
+    int opt;
+    int optionIndex = 0;
+
+    static const char* OPTSTR = "hdms:n:";
+    static const struct option OPTIONS[] = {
+        {"help", no_argument, nullptr, 'h'},
+        {"ibl-sample", required_argument, nullptr, 'n'},
+        {"ibl-size", required_argument, nullptr, 's'},
+        {"no-threads", no_argument, nullptr, 's'},
+        {"debug", no_argument, nullptr, 'd'},
+        {nullptr, 0, 0, 0} // termination of the option list
+    };
+
+    while ((opt = getopt_long(argc, argv, OPTSTR, OPTIONS, &optionIndex)) >= 0)
+    {
+        const char* arg = optarg ? optarg : "";
+        if (opt == -1)
+            break;
+
+        switch (opt)
+        {
+        default:
+        case 'h':
+            printUsage();
+            exit(0);
+            break;
+        case 'm':
+            options.nbThreads = 1;
+            break;
+        case 's':
+            options.cubemapSize = atoi(arg);
+            break;
+        case 'n':
+            options.numSamples = atoi(arg);
+            break;
+        case 'd':
+            options.debug = true;
+            break;
+        }
+    }
+    return optind;
+}
 
 int main(int argc, char** argv)
 {
-    char* file = argv[1];
-
-    if (argc < 2)
+    Options options;
+    int optionIndex = parseArgument(options, argc, argv);
+    int numArgs = argc - optionIndex;
+    if (numArgs < 1)
     {
-        printf("usage %s environment\n", argv[0]);
+        printUsage();
         return 1;
     }
+
+    options.inputFilename = argv[optionIndex];
 
     Image image;
     const char* distDir = "test";
 
-    int loadImageResult = envUtils::loadImage(image, file);
+    int loadImageResult = envUtils::loadImage(image, options.inputFilename);
     if (loadImageResult == 0)
     {
 
-        int nbThread;
-        nbThread = (int)std::thread::hardware_concurrency();
-        printf("using %d threads\n", nbThread);
+        if (!options.nbThreads) {
+            options.nbThreads = (int)std::thread::hardware_concurrency();
+        }
+        printf("using %d threads\n", options.nbThreads);
 
         // needs to be sure that the panorama is pow of 2
         Cubemap cm;
-        envUtils::createCubemap(cm, 256);
+        envUtils::createCubemap(cm, options.cubemapSize);
 
         envUtils::clampImage(image, 255);
 
-        envUtils::writeThumbnail(distDir, "thumbnail", image, 256, 128);
-        envUtils::equirectangularToCubemap(cm, image, nbThread);
+        envUtils::writeThumbnail(distDir, "thumbnail", image, options.thumbnailSize, options.thumbnailSize / 2);
+        envUtils::equirectangularToCubemap(cm, image, options.nbThreads);
 
-        if (debug)
+        if (options.debug)
             envUtils::writeCubemap_hdr(distDir, "input", cm);
 
         CubemapMipMap cmMipMap;
         envUtils::createCubemapMipMap(cmMipMap, cm);
 
-        if (debug)
+        if (options.debug)
             envUtils::writeCubemapMipMapFaces_hdr(distDir, "mipmap", cmMipMap);
 
         CubemapMipMap cmPrefilter;
-        envUtils::prefilterCubemapGGX(cmPrefilter, cmMipMap, 4096, nbThread);
+        envUtils::prefilterCubemapGGX(cmPrefilter, cmMipMap, options.numSamples, options.nbThreads);
 
-        if (debug)
+        if (options.debug)
             envUtils::writeCubemapMipMapFaces_hdr(distDir, "prefilter", cmPrefilter);
 
         Spherical spherical;
@@ -58,7 +141,7 @@ int main(int argc, char** argv)
         envUtils::writeSpherical_json(distDir, "spherical", spherical);
         envUtils::writeCubemapMipMap_luv(distDir, "prefilter", cmPrefilter);
 
-        if (debug)
+        if (options.debug)
         {
             CubemapMipMap cmDecode;
             envUtils::readCubemapMipMap_luv(cmDecode, "test/prefilter.luv");
@@ -67,7 +150,6 @@ int main(int argc, char** argv)
         }
 
         envUtils::writeCubemap_luv(distDir, "background", cmPrefilter.levels[2]);
-
 
         envUtils::freeCubemapMipMap(cmPrefilter);
         envUtils::freeCubemapMipMap(cmMipMap);
--- src/threadLines.h
+++ src/threadLines.h
@@ -3,43 +3,41 @@
 #include <math.h>
 #include <thread>
 
-#define USE_THREAD
-
 template <class T, class ArgObject> void threadLines(T* func, ArgObject context, int nbLines, int nbThread)
 {
-
-#ifdef USE_THREAD
-    std::thread threadList[64];
-
-    if (nbThread > 64)
-        nbThread = 64;
-
-    if (nbLines < nbThread)
+    if (nbThread > 1)
     {
-        nbThread = nbLines;
-    }
+        std::thread threadList[64];
 
-    float linesPerThread = nbLines / nbThread;
-    int startY = 0;
-    int stopY;
-    for (int i = 0; i < nbThread; i++)
+        if (nbThread > 64)
+            nbThread = 64;
+
+        if (nbLines < nbThread)
+        {
+            nbThread = nbLines;
+        }
+
+        float linesPerThread = nbLines / nbThread;
+        int startY = 0;
+        int stopY;
+        for (int i = 0; i < nbThread; i++)
+        {
+            stopY = startY + (int)ceilf(linesPerThread);
+            if (stopY > nbLines - 1)
+                stopY = nbLines - 1;
+
+            threadList[i] = std::thread(func, context, startY, stopY);
+
+            startY = stopY + 1;
+        }
+
+        for (int i = 0; i < nbThread; i++)
+        {
+            threadList[i].join();
+        }
+    }
+    else
     {
-        stopY = startY + (int)ceilf(linesPerThread);
-        if (stopY > nbLines - 1)
-            stopY = nbLines - 1;
-
-        threadList[i] = std::thread(func, context, startY, stopY);
-
-        startY = stopY + 1;
+        (*func)(context, 0, nbLines - 1);
     }
-
-    for (int i = 0; i < nbThread; i++)
-    {
-        threadList[i].join();
-    }
-#else
-    for (int i = 0; i < nbThread; i++)
-        break;
-    (*func)(context, 0, nbLines - 1);
-#endif
 }
